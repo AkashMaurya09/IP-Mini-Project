a43957e384e09f7b05bc971ef5b62007fa93ce4b
devdattakhoche <dkhoche70@gmail.com>
2020-11-09 21:44:34 +0530

    :art: Adding Files

--- pages/signup.php
+++ pages/signup.php
@@ -5,15 +5,21 @@
   <main>
     <h1>This is the signup page</h1>  
     <form action="includes/signup.php" method="post">
-      <input type="text" name="name" placeholder="Name">
-      <input type="number" name="number" placeholder="Phone Number">
-      <input type="text" name="address" placeholder="Address">
-      <input type="text" name="uid" placeholder="Username">
-      <input type="date" name="dob">
-      <input type="text" name="uid" placeholder="Username">
+      <input type="text" name="email" placeholder="Email">   
+      <br/>
+      <input type="text" name="uname" placeholder="Username">
+      <br/>
+      <input type="number" name="number"  placeholder="Phone Number">
+      <br/>
+      <input type="date" name="dob" placeholder="Date of Birth">
+      <br/>
       <input type="password" name="pwd" placeholder="Password">
+      <br/>
       <input type="password" name="confirm-pwd" placeholder="Confirm Password">
+      <br/>
+      <button type="submit">Register</button>
     </form>
+    
   </main>
 
 <?php 
