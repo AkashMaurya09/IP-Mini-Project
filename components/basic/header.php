efa7595a7aaa4fd0f5a1e8fd196bd667b7a5d2d7
devdattakhoche <dkhoche70@gmail.com>
2020-11-10 16:47:33 +0530

    :art: Added Logo

--- components/basic/header.php
+++ components/basic/header.php
@@ -17,7 +17,7 @@
         <input type="checkbox" id="nav-check">
         <div class="nav-header">
           <div class="nav-title">
-          <img src="../img/logo_yellow.png">
+          <img src="../img/logo/logo_yellow_horizontal.png">
           </div>
         </div>
         <div class="nav-btn">
