775f1e761bb4d7de0749632c4ecc0c44728ee34b
devdattakhoche <dkhoche70@gmail.com>
2020-12-05 08:25:42 +0530

    :art: aboutus completed

--- pages/member/aboutus.php
+++ pages/member/aboutus.php
@@ -1,11 +1,108 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="/css/user/aboutus.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php 
   require('../../components/basic/header.php')
 ?>
+    <div id='main'>
+        
+            <div class = "Center-start">
+                <h1 class="Try-free-heading">ABOUT<br>BEAST FACTORY</h1>
+</div>
+<div class="Content-center">
+<img src="../../img/logo/logo_yellow_vertical.png" alt="">
 
-  <main>
-    <h1>This is the about page</h1>  
-  </main>
+</div>
+                <div class = "Content-center"><p class="Try-free-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae obcaecati enim
+                    suscipit neque minima a harum est sequi labore repellat?</p></div>
+                <div class="Center-start"><button class="Try-free-Button">Try Free Workouts</button></div>
+            </div>
+        
 
-<?php 
+    </div>
+    <div class="Content-Class">
+        <div class="Text-Content">
+            <p><a>Workouts |</a>
+                <a> Our Coaches | </a>
+                <a> What to Expect </a>
+            </p>
+            <!-- <br> -->
+            <h1>Workouts<h2>
+                    <!-- <br> -->
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, quaerat.</p>
+        </div>
+        <div class="Workouts">
+            <div class="row">
+                <div class="column ">
+                    <div class="Workout-center">
+                        <h1>Expert Traning</h1>
+                    </div>
+
+                    <div class="Workout-center">
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure fugiat hic natus eaque eum,
+                            modi a officia praesentium esse voluptates et adipisci minus, consequatur consectetur
+                            nostrum soluta quia deseru!</p>
+                    </div>
+                    <div class="Workout-center">
+                      <button class = "Try-free-Button">Hello </button>
+                    </div>
+                    <div class="Workout-center">
+                        <img src="https://picsum.photos/300/300" alt="W3Schools.com">
+                    </div>
+                    <!-- <div class="Workout-center">
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure fugiat hic natus eaque eum,
+                            modi a officia praesentium esse voluptates et adipisci minus, consequatur consectetur
+                            nostrum soluta quia deserunt animi earum cumque! Aperiam labore accusantium molestias
+                            laudantium quisquam quo aspernatur!</p>
+                    </div> -->
+
+                </div>
+                <div class="column">
+                    <div class="Workout-center">
+                        <img src="https://picsum.photos/300/300" alt="W3Schools.com">
+                    </div>
+                    <!-- <div class="Workout-center">
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure fugiat hic natus eaque eum,
+                            modi a officia praesentium esse voluptates et adipisci minus, consequatur consectetur
+                            nostrum soluta quia deserunt animi earum cumque! Aperiam labore accusantium molestias
+                            laudantium quisquam quo aspernatur!</p>
+                    </div> -->
+                    <!-- <div class="Workout-center">
+                        <img src="https://picsum.photos/300/300" alt="W3Schools.com">
+                    </div> -->
+                    <div class="Workout-center">
+                        <h1>Expert Traning</h1>
+                    </div>
+
+                    <div class="Workout-center">
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure fugiat hic natus eaque eum,
+                            modi a officia praesentium esse voluptates et adipisci minus, consequatur consectetur
+                            nostrum soluta quia deseru!</p>
+                    </div>
+                    <div class="Workout-center">
+                      <button class = "Try-free-Button">Hello </button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="Center-start ">
+      <h1 class="expertise">Expertise</h1>
+</div>
+    <?php 
   require('../../components/basic/footer.php')
 ?>
+</body>
+
+</html>
