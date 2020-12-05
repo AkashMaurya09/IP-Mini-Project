9d5589ab581f5db9a221093521818e46d958445f
Akash Maurya <AkashMaurya09@users.noreply.github.com>
2020-12-06 02:38:57 +0530

    ⚡️ video playes according to user purchase

--- pages/member/videoPlay.php
+++ pages/member/videoPlay.php
@@ -1,5 +1,5 @@
-<?php 
-  include_once '../includes/dbh.inc.php';  
+<?php
+include_once '../includes/dbh.inc.php';
 ?>
 
 <!DOCTYPE html>
@@ -16,13 +16,13 @@
 
 <body>
 
-    <?php 
+    <?php
     require('../../components/basic/header.php')
-  ?>
+    ?>
 
-  <?php
+    <?php
     $video_id = $_GET['Video_id'];
-  ?>
+    ?>
 
     <div class="container">
         <div class="left profile">
@@ -30,20 +30,20 @@
                 <input type="submit" class="profileButton" name="dashboard" value="Dashboard" />
                 <input type="submit" + class="profileButton" name="logout" value="Logout" />
             </form>
-            <?php 
-                $memberid = $_SESSION['memberid'];
-                $sql = "Select * from Member WHERE Member_id = $memberid"; 
-                $result = mysqli_query($conn,$sql);
-                $resultCheck = mysqli_num_rows($result);
-                
-                if ($resultCheck > 0) {
-                    $row = mysqli_fetch_assoc($result);
-                }
+            <?php
+            $memberid = $_SESSION['memberid'];
+            $sql = "Select * from Member WHERE Member_id = $memberid";
+            $result = mysqli_query($conn, $sql);
+            $resultCheck = mysqli_num_rows($result);
+
+            if ($resultCheck > 0) {
+                $row = mysqli_fetch_assoc($result);
+            }
             ?>
             <div class="profileDetail">
-                <?php echo "<p><span>Name:</span> ". $row['Member_Name'] ."</p>" ?>
-                <?php echo "<p><span>Email:</span> ". $row['Member_Email']." </p>"?>
-                <?php echo "<p><span>Phone Number:</span> ". $row['Phone_Number']." </p>"?>
+                <?php echo "<p><span>Name:</span> " . $row['Member_Name'] . "</p>" ?>
+                <?php echo "<p><span>Email:</span> " . $row['Member_Email'] . " </p>" ?>
+                <?php echo "<p><span>Phone Number:</span> " . $row['Phone_Number'] . " </p>" ?>
             </div>
 
             <div class="profileImage">
@@ -54,43 +54,39 @@
 
         <div class="right">
             <div>
-                <div class = "Content-Center videoStyle">
-                <!-- <video width="600" height="400" controls>
+                <div class="Content-Center videoStyle">
+                    <!-- <video width="600" height="400" controls>
                     <source src="http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4"
                         type="video/mp4">
                     Your browser does not support the video tag.
                 </video> -->
-                <?php
+                    <?php
                     $fetchVideos = mysqli_query($conn, "SELECT * FROM Workout where Video_id=(SELECT Video_id from purchased where Member_id= $memberid);");
-                    while($row = mysqli_fetch_array($fetchVideos)){
+                    while ($row = mysqli_fetch_array($fetchVideos)) {
                         $location = $row['location'];
-                    
+
                         echo "<div >";
-                        echo "<video src='".$location."' controls width='320px' height='200px' >";
+                        echo "<video src='" . $location . "' controls width='320px' height='200px' >";
                         echo "</div>";
                     }
-                ?>
+                    ?>
                 </div>
                 <div class="Content-Center">
                     <div class="video-details">
                         <?php
-                            $fetchVideos = mysqli_query($conn, "SELECT * FROM Workout where Video_id=(SELECT Video_id from purchased where Member_id= $memberid);");
-                            while($row = mysqli_fetch_array($fetchVideos)){
-                                $location = $row['location'];
-                            
-                                echo "<h2>". print_r($row) ."</h2>";                                
-                                echo "<h2>". $row['Video_Name'] ."</h2>"; 
-                                echo "<p id='Desc'>Descpription</p>"; 
-                                echo "<h2>". $row['Description'] ."</h2>"; 
-                                echo "<h2>". $row['Video_Name'] ."</h2>"; 
-                                echo "<h2>". $row['Video_Name'] ."</h2>"; 
-                                echo "<h2>". $row['Video_Name'] ."</h2>"; 
-                            }
+                        $fetchVideos = mysqli_query($conn, "SELECT * FROM Workout where Video_id=(SELECT Video_id from purchased where Member_id= $memberid);");
+                        while ($row = mysqli_fetch_array($fetchVideos)) {
+                            $location = $row['location'];
+
+                            // echo "<h2>". print_r($row) ."</h2>";                                
+                            echo "<h2>" . $row['Video_Name'] . "</h2>";
+                            echo "<p id='Desc'>Descpription</p>";
+                            echo "<h2>" . $row['Description'] . "</h2>";
+                            echo "<p id='Tags'>Tags</p>";
+                            echo "<span id='Video-Tags'>#Full Body #Myworkout</span>";
+                            echo "<p id='Tags'>Valid till : 20-Dec-2020</p>";
+                        }
                         ?>
-                        <p id="Description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo sed neque numquam perferendis, commodi nisi vel esse assumenda vitae sit temporibus voluptatum a, quae similique sunt labore exercitationem odio delectus.</p>
-                        <p id="Tags">Tags</p>
-                        <span id="Video-Tags">#Full Body #Myworkout</span>
-                        <p id="Tags">Valid till : 20-Dec-2020</p>
                     </div>
                 </div>
             </div>
@@ -102,9 +98,7 @@
                 </div>
                 <div class="add-comment">
 
-                    <textarea placeholder="Your Comment goes here...." rows="5" name="comment[text]" id="comment_text"
-                        cols=20 class="ui-autocomplete-input" autocomplete="off" role="textbox" aria-autocomplete="list"
-                        aria-haspopup="true"></textarea>
+                    <textarea placeholder="Your Comment goes here...." rows="5" name="comment[text]" id="comment_text" cols=20 class="ui-autocomplete-input" autocomplete="off" role="textbox" aria-autocomplete="list" aria-haspopup="true"></textarea>
 
                     <div class="button_base b07_3d_double_roll">
                         <div>Publish Comment</div>
@@ -115,37 +109,37 @@
                 </div>
             </div>
             <div class="Comments-Section">
-                    <div class="Content-Center">
-                        <p id="Comments-heading">What did others say...</p>
+                <div class="Content-Center">
+                    <p id="Comments-heading">What did others say...</p>
 
-                    </div>
                 </div>
+            </div>
 
-                <div class="comment-card">
-                    
+            <div class="comment-card">
+
                 <img src="https://picsum.photos/300/300" />
-                    <p ic="username">  Devdatta Khoche</p>
-                    <div class="comment">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore officia aliquid reiciendis sit, odio maxime dignissimos laborum repellat magni tempore hic. Explicabo sit blanditiis ipsum, natus pariatur accusamus tenetur quo.</p>
-                    </div>
-                    <p class="time">Bet 5 days ago</p>
+                <p ic="username"> Devdatta Khoche</p>
+                <div class="comment">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore officia aliquid reiciendis sit, odio maxime dignissimos laborum repellat magni tempore hic. Explicabo sit blanditiis ipsum, natus pariatur accusamus tenetur quo.</p>
                 </div>
-                <div class="comment-card">
-                    
-                    <img src="https://picsum.photos/300/300" />
-                        <p ic="username">  Devdatta Khoche</p>
-                        <div class="comment">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore officia aliquid reiciendis sit, odio maxime dignissimos laborum repellat magni tempore hic. Explicabo sit blanditiis ipsum, natus pariatur accusamus tenetur quo.</p>
-                        </div>
-                        <p class="time">Bet 5 days ago</p>
-                    </div>
+                <p class="time">Bet 5 days ago</p>
+            </div>
+            <div class="comment-card">
+
+                <img src="https://picsum.photos/300/300" />
+                <p ic="username"> Devdatta Khoche</p>
+                <div class="comment">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore officia aliquid reiciendis sit, odio maxime dignissimos laborum repellat magni tempore hic. Explicabo sit blanditiis ipsum, natus pariatur accusamus tenetur quo.</p>
+                </div>
+                <p class="time">Bet 5 days ago</p>
+            </div>
         </div>
-        
-                
+
+
     </div>
 
-    <?php 
-        require('../../components/basic/footer.php')
+    <?php
+    require('../../components/basic/footer.php')
     ?>
 </body>
 
