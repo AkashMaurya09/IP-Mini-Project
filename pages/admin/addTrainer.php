6ad00339b41b59a5980508dd6db803db9ed9c818
Akash Maurya <AkashMaurya09@users.noreply.github.com>
2020-12-12 13:18:45 +0530

    ⚡️ CSS

--- pages/admin/addTrainer.php
+++ pages/admin/addTrainer.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="../../css/Trainer/editVideo.css" />
+    <link rel="stylesheet" href="\css\trainer\editVideo.css" />
 
     <title>Add Trainer</title>
 </head>
