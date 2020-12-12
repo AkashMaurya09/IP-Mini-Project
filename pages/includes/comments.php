a78c69c719b6544316ed8de14839a82583c7adac
Srajan1122 <mahabala1122@gmail.com>
2020-12-12 14:13:43 +0530

    💄 Comment ui changed

--- pages/includes/comments.php
+++ pages/includes/comments.php
@@ -5,6 +5,7 @@ session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $video_id = $_GET['Video_id'];
     $comment_query = mysqli_query($conn, "SELECT * FROM comment where Video_id='$video_id' ORDER BY timestamp DESC;");
+    echo "<div class='trainerList'>";
     while ($comment = mysqli_fetch_assoc($comment_query)) {
         $id = $comment['Member_id'];
         $sql = "Select * from Member WHERE Member_id = $id";
@@ -15,16 +16,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             $memberId = mysqli_fetch_assoc($result);
         }
 
-        // echo "<h2>" . print_r($memberId) . "</h2>";
-        echo "<div class='comment-card'>";
-        echo "<img src='" . $memberId['location'] . "' />";
-        echo "<p ic='username'> " . $memberId['Member_Name'] . "</p>";
-        echo "<div class='comment'>";
-        echo "<p>" . $comment['userComment'] . "</p>";
-        echo "</div>";
-        echo "<p class='time'>" . $comment['timestamp'] . "</p>";
-        echo "</div>";
+        echo '
+          <div class="singleTrainer">
+              <img src="' . $memberId['location'] . '" />
+              <div class="detailContent">
+                  <p>' . $memberId['Member_Name'] . '</p> 
+                  <p class="tag"></p> 
+                  <p class="description">'. $comment['userComment'] . '</p>
+              </div>
+          </div>
+          ';
+
+        // echo "<div class='comment-card'>";
+        // echo "<img src='" . $memberId['location'] . "' />";
+        // echo "<p id='username'> " . $memberId['Member_Name'] . "</p>";
+        // echo "<div class='comment'>";
+        // echo "<p>" . $comment['userComment'] . "</p>";
+        // echo "</div>";
+        // echo "<p class='time'>" . $comment['timestamp'] . "</p>";
+        // echo "</div>";
     }
+    echo "</div>";
 }
 
 if (isset($_POST["comment"])) {
--- pages/member/videoPlay.php
+++ pages/member/videoPlay.php
@@ -10,6 +10,7 @@ include_once '../includes/dbh.inc.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="/css/trainer/editVideo.css" />
     <link rel="stylesheet" href="/css/user/videoPlay.css" />
+    <link rel="stylesheet" href="/css/trainer/trainerList.css" />
     <link rel="stylesheet" href="/css/user/comments.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="/js/comment.js"></script>
@@ -57,12 +58,17 @@ include_once '../includes/dbh.inc.php';
 
         <div class="right">
             <div>
+                <div class="Content-Center">
+                    <div class="video-details">
+                        <?php
+                        $fetchVideos = mysqli_query($conn, "SELECT * FROM Workout where Video_id='$video_id';");
+                        while ($_videoRow = mysqli_fetch_array($fetchVideos)) {
+                            echo "<h2>" . $_videoRow['Video_Name'] . "</h2>";
+                        }
+                        ?>
+                    </div>
+                </div>
                 <div class="Content-Center videoStyle">
-                    <!-- <video width="600" height="400" controls>
-                    <source src="http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4"
-                        type="video/mp4">
-                    Your browser does not support the video tag.
-                </video> -->
                     <?php
                     $fetchVideos = mysqli_query($conn, "SELECT * FROM Workout where Video_id='$video_id';");
                     while ($row = mysqli_fetch_array($fetchVideos)) {
@@ -81,14 +87,8 @@ include_once '../includes/dbh.inc.php';
                         while ($_videoRow = mysqli_fetch_array($fetchVideos)) {
                             $location = $_videoRow['location'];
                             $videoid = $_videoRow['Video_id'];
-
-                            // echo "<h2>". print_r($_videoRow) ."</h2>";                                
-                            echo "<h2>" . $_videoRow['Video_Name'] . "</h2>";
-                            echo "<p id='Desc'>Descpription</p>";
-                            echo "<h2>" . $_videoRow['Description'] . "</h2>";
-                            echo "<p id='Tags'>Tags</p>";
                             echo "<span id='Video-Tags'>#Full Body #Myworkout</span>";
-                            echo "<p id='Tags'>Valid till : 20-Dec-2020</p>";
+                            echo "<h2 id='Description'>" . $_videoRow['Description'] . "</h2>";
                         }
                         ?>
                     </div>
@@ -97,27 +97,29 @@ include_once '../includes/dbh.inc.php';
             <!-- ---------------------Add Comments-------------------------- -->
             <div>
                 <div class="Comments-Section">
-                    <div class="Content-Center">
+                    <div>
                         <p id="Comments-heading">Comments</p>
                     </div>
                 </div>
                 <div class="add-comment">
-                    <!-- <form method="post" action=""> -->
-                        <textarea required placeholder="Your Comment goes here...." rows="5" name="comment" id="comment_text" cols=20 class="ui-autocomplete-input" autocomplete="off" role="textbox" aria-autocomplete="list" aria-haspopup="true"></textarea>
-                        <button type='submit'  id="add_com" name='add_comment'>Add Comment</button>
-                    <!-- </form> -->
+                    <textarea required placeholder="Your Comment goes here...." rows="5" name="comment"
+                        id="comment_text" cols=20 class="ui-autocomplete-input" autocomplete="off" role="textbox"
+                        aria-autocomplete="list" aria-haspopup="true"></textarea>
+                    <button type='submit' id="add_com" name='add_comment'>Add Comment</button>
                 </div>
             </div>
             <!-- ---------------------Display Comments-------------------------- -->
-            <input type="hidden" value=<?php echo $video_id; ?> id="video_id"/>
-            <div class="Comments-Section">
-                <div class="Content-Center">
+            <input type="hidden" value=<?php echo $video_id; ?> id="video_id" />
+            <!-- <div class="Comments-Section">
+                <div >
                     <p id="Comments-heading">What did others say...</p>
-
                 </div>
-            </div>
+            </div> -->
+<!-- 
+            <div class="trainerList">
+                <div id="display"></div>
+            </div> -->
             
-            <div id="display"></div>
         </div>
 
 
